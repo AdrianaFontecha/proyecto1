ad09ac9eb3779d518b753283632fafe9337e2d32
AdrianaFontecha <adrianafontecha1122@gmail.com>
2025-10-05 17:56:07 -0500

    proyecto

---
+++ frontend/src/presentation/views/register/ViewModel.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+  import { RegisterAuthUseCase } from '../../../domain/useCases/auth/RegisterAuth';
+
+  const RegisterViewModel = () => {
+      const [values, setValues] = useState({
+        name: '',
+        lastname: '',
+        phone: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+      const onChange = (property: string, value: any) => {
+        setValues({ ...values, [property]: value });
+      };
+      const register = async () => {
+        const response = await RegisterAuthUseCase(values);
+        console.log('Result: ' + JSON.stringify(response));
+      }
+      return {
+        ...values,
+        onChange,
+        register
+      };
+  }
+
+  export default RegisterViewModel;
